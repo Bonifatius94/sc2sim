f05c1a136ed7619a3d2d4bec01fb22aad55b36ef
Marco Tröster <marco@troester-gmbh.de>
2021-12-07 17:16:34 +0100

    reduced world size

--- sc2sim/__main__.py
+++ sc2sim/__main__.py
@@ -6,7 +6,7 @@ from sc2sim.sc2_runner import SC2GameSession
 
 def main():
     """Launch the main procedure"""
-    env = SC2Env()
+    env = SC2Env(world_width=32, world_height=32)
     agent = QLAgent()
     runner = SC2GameSession(env, agent, 20000, 1000)
     runner.run()
