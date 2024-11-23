d11804970efe1325745acd0c10bbd7c5d93bc2b5
Marco Tröster <marco@troester-gmbh.de>
2024-11-23 22:52:04 +0100

    represent state as discretized sectors around the agent

--- sc2sim/sc2_ql_agent.py
+++ sc2sim/sc2_ql_agent.py
@@ -3,6 +3,7 @@ playing the StarCraft II MoveToBeacon game"""
 
 from typing import Dict, Tuple
 from dataclasses import dataclass, field
+from math import atan2, pi
 import numpy as np
 from sc2sim.sc2_environment import SC2State, SC2Experience, SC2Action
 
@@ -14,7 +15,7 @@ class QLAgent:
     min_epsilon: float=0.1
     epsilon_decay_steps=500*500
     gamma: float=0.99
-    model: Dict[Tuple[int, int], np.ndarray] = field(default_factory=dict)
+    model: Dict[int, np.ndarray] = field(default_factory=dict)
     action_space: int=8
     step: int=0
 
@@ -48,9 +49,23 @@ class QLAgent:
             (1 - self.step / self.epsilon_decay_steps)) + self.min_epsilon
 
     @staticmethod
-    def _encode_state(state: SC2State) -> Tuple[int, int]:
-        return (state.beacon_pos[0] - state.marine_pos[0],
-                state.beacon_pos[1] - state.marine_pos[1])
+    def _encode_state(state: SC2State) -> int:
+
+        def state_as_vector(state: SC2State) -> Tuple[float, float]:
+            dx = state.beacon_pos[0] - state.marine_pos[0]
+            dy = state.beacon_pos[1] - state.marine_pos[1]
+            return dx, dy
+
+        def direction_of_vector_in_rad(vec: Tuple[float, float]) -> float:
+            return atan2(vec[1], vec[0]) + pi
+
+        def discretize_angle(angle_rad: float, num_sectors: int) -> int:
+            return int(angle_rad / (2*pi) * num_sectors) % num_sectors
+
+        return discretize_angle(
+            direction_of_vector_in_rad(state_as_vector(state)),
+            num_sectors=32
+        )
 
     def _init_state(self, state: Tuple[int, int]):
         self.model[state] = np.random.uniform(size=self.action_space)
